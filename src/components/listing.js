31fff5f80c7e4b6a5ec4858ffd44f8438875ab63
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2025-06-24 09:23:00 +0100

    refresh studio

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
-import {device} from '../components/styles/mediaQueries'
-import { BsLink45Deg } from 'react-icons/bs';
+import { device } from "../components/styles/mediaQueries"
+import { BsLink45Deg } from "react-icons/bs"
 
-const Listing = styled.li` 
+const Listing = styled.li`
   display: flex;
   justify-content: space-between;
-  padding: .5rem 1rem;
+  padding: 0.5rem 1rem;
   flex-direction: column;
   margin: 0;
   &:not(:last-child) {
@@ -17,7 +17,7 @@ const Listing = styled.li`
   }
 `
 
-const Date = styled.span` 
+const Date = styled.span`
   text-align: center;
   flex: 1;
   text-transform: lowercase;
@@ -26,21 +26,21 @@ const Date = styled.span`
   }
 `
 
-const Details = styled.div` 
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    margin-bottom: 0.25rem;
-    align-items: center;
-    @media ${device.mediaMinMedium} {
-        text-align: left;
-        margin-bottom: 0;
-        flex-direction: row;
+const Details = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 0.25rem;
+  align-items: center;
+  @media ${device.mediaMinMedium} {
+    text-align: left;
+    margin-bottom: 0;
+    flex-direction: row;
   }
 `
 
-const Artist = styled.span` 
+const Artist = styled.span`
   /* font-weight: bold; */
   display: flex;
   align-items: center;
@@ -48,49 +48,75 @@ const Artist = styled.span`
   text-transform: lowercase;
 `
 
-const TicketButton = styled.a` 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: fit-content;
-    font-size: 0.875rem;
-    text-transform: lowercase;
-    font-weight: normal;
+const TicketButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  font-size: 0.875rem;
+  text-transform: lowercase;
+  font-weight: normal;
 
-    &:hover {
-     text-decoration: underline;
-    }
+  &:hover {
+    text-decoration: underline;
+  }
 `
 
-const ListingItem = ({listingInfo}) => {
-    const {eventDate, ticketLink, artistUrl, listing} = listingInfo;
-    const [finalDate, setfinalDate] = useState()
+const ListingItem = ({ listingInfo }) => {
+  const { eventDate, ticketLink, artistUrl, listing } = listingInfo
+  const [finalDate, setfinalDate] = useState()
 
-    console.log('artistUrl:', artistUrl)
+  console.log("artistUrl:", artistUrl)
 
-    // Check if window is defined (so if in the browser or in node.js).
-    const isBrowser = typeof window !== "undefined"
+  // Check if window is defined (so if in the browser or in node.js).
+  const isBrowser = typeof window !== "undefined"
 
-    useEffect(() => {
-      const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      const dateObj = new window.Date(eventDate);
-      const month = months[dateObj.getMonth()];
-      const dateStringAsArray = dateObj.toDateString().split(" ");
-      const formattedDate = `${dateStringAsArray[0]} ${dateStringAsArray[2]}, ${month}`;
-      setfinalDate(formattedDate)
-    }, [isBrowser, eventDate])
+  useEffect(() => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]
+    const dateObj = new window.Date(eventDate)
+    const month = months[dateObj.getMonth()]
+    const dateStringAsArray = dateObj.toDateString().split(" ")
+    const formattedDate = `${dateStringAsArray[0]} ${dateStringAsArray[2]}, ${month}`
+    setfinalDate(formattedDate)
+  }, [isBrowser, eventDate])
 
-    return (
-        <Listing>
-            <Details>
-                <Artist>
-                  {artistUrl ? <a href={artistUrl} target="_blank" rel='noreferrer'>{listing}</a> : `${listing}`}
-                </Artist>
-                {ticketLink ? <>- &nbsp;<TicketButton href={ticketLink} target='_blank' rel="noreferrer">Buy ticket </TicketButton></> : null}
-            </Details>
-            <Date>{finalDate}</Date>
-        </Listing>
-    )
+  return (
+    <Listing>
+      <Details>
+        <Artist>
+          {artistUrl ? (
+            <a href={artistUrl} target="_blank" rel="noreferrer">
+              {listing}
+            </a>
+          ) : (
+            `${listing}`
+          )}
+        </Artist>
+        {ticketLink ? (
+          <>
+            - &nbsp;
+            <TicketButton href={ticketLink} target="_blank" rel="noreferrer">
+              Buy ticket{" "}
+            </TicketButton>
+          </>
+        ) : null}
+      </Details>
+      <Date>{finalDate}</Date>
+    </Listing>
+  )
 }
 
 export default ListingItem
